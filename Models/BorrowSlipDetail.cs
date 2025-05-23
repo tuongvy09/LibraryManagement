64e0d04f5f4a04183d7831e4fc0399aaec100dae
hngan1402 <125951594+hngan1402@users.noreply.github.com>
2025-05-23 16:04:41 +0700

    b1
    bbb1

---
+++ Models/BorrowSlipDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Models
+{
+    public class BorrowSlipDetail
+    {
+        public int SlipDetailID { get; set; }
+        public int SlipID { get; set; }
+        public int BookID { get; set; }
+        public bool IsReturned { get; set; } // Trạng thái đã trả hay chưa
+    }
+}
