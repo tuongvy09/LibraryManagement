2854fc3154537b7a3e30ce04aca0812e768b6298
hngan1402 <125951594+hngan1402@users.noreply.github.com>
2025-05-31 19:51:50 +0700

    commit report _2
    commit report _2

---
+++ UI/FormThongKeReport.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace LibraryManagement.UI
+{
+    partial class FormThongKeReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Location = new System.Drawing.Point(9, 9);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(978, 512);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // FormThongKeReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(999, 533);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "FormThongKeReport";
+            this.Text = "FormThongKeReport";
+            this.Load += new System.EventHandler(this.FormThongKeReport_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+    }
+}
